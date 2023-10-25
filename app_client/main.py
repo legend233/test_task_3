48aea9b7e90cfda7b1a8d60705ff124ca67827e5
legend23 <yu@vtechnology.ru>
2023-10-25 22:46:02 +0500

    start edit console interface

--- app_client/main.py
+++ app_client/main.py
@@ -1,58 +1,19 @@
-import requests
-from settings import SERVER, PORT
+from functions import (add_book, delete_book, edit_book,
+                        add_user, delete_user, edit_user,
+                        add_getbook_to_journal, add_returnbook_to_journal,
+                        total_count_books, total_count_users,
+                        total_count_rent, total_count_notreturn,
+                        total_last_date, max_reading_author, top_genres,
+                        top_genres_for_users, total_delays)
 
-HOST = f"http://{SERVER}:{PORT}"
+from rich.table import Table
+from rich.console import Console
+from rich import print
+
+def main():
+    for _ in total_count_users():
+        print(_)
 
 
-
-def add_book() -> list:
-    pass
-
-
-def delete_book(id: int) -> list:
-    pass
-
-def edit_book() -> list:
-    pass
-
-def add_user() -> list:
-    pass
-
-def delete_user() -> list:
-    pass
-
-def edit_user() -> list:
-    pass
-
-def add_getbook_to_journal() -> list:
-    pass
-
-def add_returnbook_to_journal() -> list:
-    pass
-
-def total_count_books() -> list:
-    return requests.get(HOST + "/total_count_books").json()
-
-def total_count_users() -> list:
-    return requests.get(HOST + "/total_count_users").json()
-
-def total_count_rent() -> list:
-    return requests.get(HOST + "/total_count_rent").json()
-
-def total_count_notreturn() -> list:
-    return requests.get(HOST + "/total_count_notreturn").json()
-
-def total_last_date() -> list:
-    return requests.get(HOST + "/total_last_date").json()
-
-def max_reading_author() -> list:
-    return requests.get(HOST + "/max_reading_author").json()
-
-def top_genres() -> list:
-    return requests.get(HOST + "/top_genres").json()
-
-def top_genres_for_users() -> list:
-    return requests.get(HOST + "/top_genres_for_users").json()
-
-def total_delays() -> list:
-    return requests.get(HOST + "/total_delays").json()
+if __name__ == "__main__":
+    main()
