d2bbbef934186af6abea68f34e0d63d955db0da3
legend23 <yu@vtechnology.ru>
2023-10-25 22:44:18 +0500

    все ручки готовы кроме create_tables

--- app_db/main.py
+++ app_db/main.py
@@ -18,44 +18,36 @@ def create_tables_():
     return create_tables()
 
 @app.get("/add_book")
-def add_book_():
-    pass
-    return add_book()
+def add_book_(title: str, author: str, genre: str):
+    return add_book(title, author, genre)
 
 @app.get("/delete_book")
-def delete_book_():
-    pass
-    return delete_book()
+def delete_book_(id: int):
+    return delete_book(id)
 
 @app.get("/edit_book")
-def edit_book_():
-    pass
-    return edit_book()
+def edit_book_(id: int, title: str = None, author: str = None, genre: str = None):
+    return edit_book(id, title, author, genre)
 
 @app.get("/add_user")
-def add_user_():
-    pass
-    return add_user()
+def add_user_(first_name: str, last_name: str):
+    return add_user(first_name, last_name)
 
 @app.get("/delete_user")
-def delete_user_():
-    pass
-    return delete_user()
+def delete_user_(id: int):
+    return delete_user(id)
 
 @app.get("/edit_user")
-def edit_user_():
-    pass
-    return edit_user()
+def edit_user_(id: int, first_name: str = None, last_name: str = None):
+    return edit_user(id, first_name, last_name)
 
 @app.get("/add_getbook_to_journal")
-def add_getbook_to_journal_():
-    pass
-    return add_getbook_to_journal()
+def add_getbook_to_journal_(book_id: int, user_id: int, days_to_return: int = 14):
+    return add_getbook_to_journal(book_id, user_id, days_to_return)
 
 @app.get("/add_returnbook_to_journal")
-def add_returnbook_to_journal_():
-    pass
-    return add_returnbook_to_journal()
+def add_returnbook_to_journal_(book_id: int):
+    return add_returnbook_to_journal(book_id)
 
 @app.get("/total_count_books")
 def total_count_books_():
